5cad1eb36312c877f952d63f9777b5587545904b
Magnus Nilsson <magnus@tenco.se>
2016-08-22 08:27:10 +0000

    added soft deletes to users table

--- app/User.php
+++ app/User.php
@@ -4,10 +4,15 @@ namespace tencotools;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class User extends Authenticatable
 {
 
-    
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
     /**
      * The attributes that are mass assignable.
      *
